880f39df818d149671fca10d18e60828d59378fc
mostafa khazaei <mostafakhazaei@live.com>
2024-10-15 16:13:50 +0330

    Privated LastName

--- MoviesAPI/MoviesAPI/GitRemotePractice/Student.cs
+++ MoviesAPI/MoviesAPI/GitRemotePractice/Student.cs
@@ -5,7 +5,7 @@ public class Student
     [Required]
     public int Id { get; set; }
     public string FirstName { get; set; }
-    public string LastName { get; set; }
+    public string LastName { get; private set; }
     public string FatherName { get; private set; }
     public string City { get; private set; }
 }
