fa940c3165d5aafe63085d28fa7a3c0e77f279f5
mostafa khazaei <mostafakhazaei@live.com>
2024-10-15 15:59:19 +0330

    Father Name Prop Added To Student

--- MoviesAPI/MoviesAPI/GitRemotePractice/Student.cs
+++ MoviesAPI/MoviesAPI/GitRemotePractice/Student.cs
@@ -6,4 +6,5 @@ public class Student
     public int Id { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
+    public string FatherName { get; set; }
 }
