c45cef350def6716d54805e4f49bb11db28dc0d3
mostafa khazaei <mostafakhazaei@live.com>
2024-10-15 16:06:12 +0330

    City Added to Student

--- MoviesAPI/MoviesAPI/GitRemotePractice/Student.cs
+++ MoviesAPI/MoviesAPI/GitRemotePractice/Student.cs
@@ -7,4 +7,5 @@ public class Student
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string FatherName { get; set; }
+    public string City { get; set; }
 }
